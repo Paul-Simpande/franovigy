5c10be55024f8e480666ab9469302445e8e1a64b
Paul-Simpande <simpandepaul03@gmail.com>
2025-06-19 12:58:33 +0200

    Created a home page and in the homepage I created the Latest post section edited it to complete, buttons are not active yet.

--- franovigy-website/src/components/pages/homepage/home.jsx
+++ franovigy-website/src/components/pages/homepage/home.jsx
@@ -1,10 +1,15 @@
 
 import './home.css';
+import { FaRegHeart } from "react-icons/fa";
 import heroImage from '../../../assets/hero-ban-image.png';
+import agricultureImage from '../../../assets/agriculture_post.avif';
+import construtorImage from '../../../assets/construction_post.avif';
+import transprtImage from '../../../assets/transport_post.avif';
 
 function Home() {
     return (
         <>
+            {/*============== HERO SECTION ==============*/}
             <section className="section hero">
                 <div className="left-col">
                     <div className="hero-title">
@@ -23,6 +28,83 @@ function Home() {
                 </div>
                 <div className="right-col">
                     <img src={heroImage} alt="image" />
+                </div>
+            </section>
+
+            {/*============== LATEST POST SECTION ==============*/}
+            <section className="section latest-post">
+                <div className="latest-post-title">
+                    <h1>Latest Post</h1>
+                </div>
+                <div className="post-list">
+                    <div className="col">
+                        <div className="img-col">
+                            <img src={agricultureImage} alt="image" />
+                        </div>
+                        <div className="detail-col">
+                            <p>Paul Simpande</p>
+                            <h1>Empowering Local Farmers:</h1>
+                            <h1>Franovigy Enterprises Agriculture Solutions</h1>
+                            <p>In the heart of Lusaka, Zambia, stands a company</p>
+                            <hr/>
+                            <div className="post-reaction">
+
+                                <div className="reaction">
+                                    <div className="eye">
+                                        <div className="eye-bow"></div>
+                                    </div>
+                                    <p>0</p>
+                                </div>
+                                <div className="spacer"></div>
+                                <div className="heart"><FaRegHeart /></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="img-col">
+                            <img src={construtorImage} alt="image" />
+                        </div>
+                        <div className="detail-col">
+                            <p>Paul Simpande</p>
+                            <h1>Building a Sustainable Future:</h1>
+                            <h1>Franovigy Enterprises Construction Services</h1>
+                            <p>In the heart of Lusaka, Zambia, stands a company</p>
+                            <hr/>
+                            <div className="post-reaction">
+                                <div className="reaction">
+                                    <div className="eye">
+                                        <div className="eye-bow"></div>
+                                    </div>
+                                    <p>0</p>
+                                </div>
+                                <div className="spacer"></div>
+                                <div className="heart"><FaRegHeart /></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="img-col">
+                            <img src={transprtImage} alt="image" />
+                        </div>
+                        <div className="detail-col">
+                            <p>Paul Simpande</p>
+                            <h1>Optimizing Logistics Solutions:</h1>
+                            <h1>Franovigy Enterprises Transport Services</h1>
+                            <p>In the heart of Lusaka, Zambia, stands a company</p>
+                            <hr/>
+                            <div className="post-reaction">
+
+                                <div className="reaction">
+                                    <div className="eye">
+                                        <div className="eye-bow"></div>
+                                    </div>
+                                    <p>0</p>
+                                </div>
+                                <div className="spacer"></div>
+                                <div className="heart"><FaRegHeart /></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
