feaa296625d82d1c10e68d275116a4f59ee70da0
Paul-Simpande <simpandepaul03@gmail.com>
2025-06-25 18:33:53 +0200

    Edited the home-page, footer and header, i added the scroll effects to each component

--- franovigy-website/src/components/footer/footer.jsx
+++ franovigy-website/src/components/footer/footer.jsx
@@ -2,17 +2,39 @@ import {Link} from 'react-router-dom';
 import { FaFacebookF, FaInstagram, FaTiktok } from "react-icons/fa";
 import { BsTwitterX } from "react-icons/bs";
 import addFormImage from "../../assets/add_form.png"
-
+import '../../scrollAnimation/scrollAnimation.css';
 import './footer.css'
+import {useEffect} from "react";
 
 function Footer() {
+
+    useEffect(() => {
+        const elements = document.querySelectorAll('.scroll-animate');
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add('show');
+                } else {
+                    entry.target.classList.remove('show');
+                }
+            });
+        }, { threshold: 0.2 });
+
+        elements.forEach(el => observer.observe(el));
+
+        return () => {
+            elements.forEach(el => observer.unobserve(el));
+        };
+    }, []);
+
     return(
         <>
             <footer>
                 <div className="container">
                     <div className="col col1">
-                        <Link to={"/"}><p>Franovigy Enterprises Limited</p></Link>
-                        <div className="address">
+                        <Link to={"/"} className="scroll-animate left"><p>Franovigy Enterprises Limited</p></Link>
+                        <div className="address scroll-animate left">
                             <p>+260 97 947 4830</p>
                             <p>info@mysite.com</p>
                             <p>Plot No. 6248, NAPSA Building</p>
@@ -20,20 +42,20 @@ function Footer() {
                             <p>Lusaka, Zambia</p>
                         </div>
                         <div className="media-links">
-                            <a href="http://www.facebook.com"><FaFacebookF /></a>
-                            <a><FaInstagram /></a>
-                            <a><BsTwitterX /></a>
-                            <a><FaTiktok /></a>
+                            <a className="scroll-animate up" href="http://www.facebook.com"><FaFacebookF /></a>
+                            <a className="scroll-animate up"><FaInstagram /></a>
+                            <a className="scroll-animate up"><BsTwitterX /></a>
+                            <a className="scroll-animate up"><FaTiktok /></a>
                         </div>
                     </div>
                     <div className="col col2">
-                        <h3>Your Success Partner</h3>
+                        <h3 className="scroll-animate right">Your Success Partner</h3>
                         <Link>
                             <div className="form">
-                                <div className="img">
+                                <div className="img scroll-animate right">
                                     <img className="form-img" src={addFormImage} alt="image"/>
                                 </div>
-                                <div className="info">
+                                <div className="info scroll-animate right">
                                     <p><b>Add Form</b></p>
                                     <p>Choose an existing form or create a new one.</p>
                                     <p>Click Here</p>
@@ -41,12 +63,12 @@ function Footer() {
                             </div>
                         </Link>
                         <div className="legal">
-                            <Link to={"/"}>Privacy Policy</Link>
-                            <Link to={"/"}>Accessibility Statement</Link>
-                            <Link to={"/"}>Terms & Conditions</Link>
-                            <Link to={"/"}>Refund Policy</Link>
+                            <Link to={"/"} className="scroll-animate right">Privacy Policy</Link>
+                            <Link to={"/"} className="scroll-animate right">Accessibility Statement</Link>
+                            <Link to={"/"} className="scroll-animate right">Terms & Conditions</Link>
+                            <Link to={"/"} className="scroll-animate right">Refund Policy</Link>
                         </div>
-                        <p>&#169;2025 by Franovigy Enterprises Limited.</p>
+                        <p className="scroll-animate down">&#169;2025 by Franovigy Enterprises Limited.</p>
                     </div>
                 </div>
             </footer>
