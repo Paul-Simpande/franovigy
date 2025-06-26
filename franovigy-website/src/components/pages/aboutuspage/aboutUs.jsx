b1b474df30bc7bee3de9f1c74e044125a3f279aa
Paul-Simpande <simpandepaul03@gmail.com>
2025-06-26 12:52:25 +0200

    Made the about us page and added its css but the page is not active yet

---
+++ franovigy-website/src/components/pages/aboutuspage/aboutUs.jsx
@@ -0,0 +1,41 @@
+import './aboutUs.css'
+
+function AboutUs() {
+    return (
+        <>
+            {/*============== ABOUT SECTION ==============*/}
+            <section className="aboutus">
+                <div className="container">
+                    <div className="row1">
+                        <p className="p-title">Crafting Sustainable Solutions</p>
+                        <p>Our Vision</p>
+                    </div>
+                    <div className="row2">
+                        <div className="col1">
+                            <p>At Franovigy Enterprises Limited, we are
+                                dedicated to providing quality services
+                                in construction, agriculture, transport,
+                                and logistics sectors. Our commitment
+                                to excellence, durability, and timeliness
+                                sets us apart in the industry. We pride
+                                ourselves on empowering local farmers
+                                and contributing to national food
+                                security through sustainable and
+                                innovative practices. Our mission is to
+                                make a positive impact by creating
+                                lasting solutions that benefit both our
+                                clients and the community.</p>
+                        </div>
+                        <div className="col2">
+                            <p>With a focus on quality,
+                            sustainability, and reliability, we strive
+                            to be a trusted partner for all your
+                            business needs.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+}
+export default AboutUs;
